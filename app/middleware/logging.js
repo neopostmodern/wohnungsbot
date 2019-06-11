79a7cace900b9613f72603afb843b2ad559f217d
neopostmodern <clemens@neopostmodern.com>
2019-06-11 20:29:13 +0200

    Improved logging of actions on main thread by limiting length of displayed payload

--- app/middleware/logging.js
+++ app/middleware/logging.js
@@ -4,7 +4,15 @@ import type { Action, Dispatch, Store } from '../reducers/types';
 
 // eslint-disable-next-line no-unused-vars
 export default (store: Store) => (next: Dispatch) => (action: Action) => {
-  console.log(action);
+  if (action.payload && JSON.stringify(action.payload).length > 100) {
+    console.log(
+      Object.assign({}, action, {
+        payload: `${JSON.stringify(action.payload).substr(0, 100)}...`
+      })
+    );
+  } else {
+    console.log(action);
+  }
 
   return next(action);
 };
