neopostmodern/wohnungsbot

commit 6f671e9b0d14b7160c52375c8c5430f1af38f868

Refactored configuration data type, all filter data now in a sub-type/object Vastly increases performance on flat re-assessments since they can now run only when the filter changed

app/reducers/configuration.js
@@ -34,13 +34,28 @@ export type configurationBoolean =
   | 'onlyOldBuilding'
   | 'onlyUnfurnished';
 
+export type Filter = {|
+  postcodes: Array<string>,
+  maximumRent?: ?number,
+  minimumArea?: ?number,
+  minimumRooms?: ?number,
+  maximumRooms?: ?number,
+  onlyOldBuilding: boolean,
+  onlyUnfurnished: boolean,
+  hasWBS: boolean,
+  mustHaveBalcony: boolean,
+  mustHaveKitchenette: boolean,
+  noKitchenette: boolean,
+  floors: Array<number>
+|};
+
 export const SALUTATIONS = {
   FRAU: 'Frau',
   HERR: 'Herr'
 };
 export type Salutation = $Values<typeof SALUTATIONS>;
 
-export type ContactData = {
+export type ContactData = {|
   salutation: Salutation,
   firstName: string,
   lastName: string,
@@ -50,7 +65,7 @@ export type ContactData = {
   houseNumber: string,
   postcode: string,
   city: string
-};
+|};
 
 export type DataPolicies = {
   flatViewingNotificationMails: boolean,
@@ -86,58 +101,45 @@ export const EMPLOYMENT_STATUS = {
 };
 export type EmploymentStatus = $Values<typeof EMPLOYMENT_STATUS>;
 
-export type AdditionalInformation = {
+export type AdditionalInformation = {|
   moveInWhen: MoveInWhen,
   moveInWho: MoveInWho,
   animals: string,
   employmentStatus: EmploymentStatus,
   income: ?number,
   hasDocumentsReady: boolean
-};
+|};
 
-export type configurationStateType = {
+export type Configuration = {|
   stage: number,
   loaded: boolean,
+  filter: Filter,
   searchUrl?: string,
-  postcodes: Array<string>,
-  maximumRent?: ?number,
-  minimumArea?: ?number,
-  minimumRooms?: ?number,
-  maximumRooms?: ?number,
-  onlyOldBuilding: boolean,
-  onlyUnfurnished: boolean,
-  hasWBS: boolean,
-  mustHaveBalcony: boolean,
-  mustHaveKitchenette: boolean,
-  noKitchenette: boolean,
-  floors: Array<number>,
   applicationText: string,
   contactData: ContactData,
   additionalInformation: AdditionalInformation,
   policies: DataPolicies
-};
+|};
 
-export const getConfigurationHash = (
-  configurationState: configurationStateType
+export const getConfigurationFilterHash = (
+  configurationState: Configuration
 ): number => {
-  const staticConfigurationState = Object.assign({}, configurationState);
-  delete staticConfigurationState.loaded;
-  delete staticConfigurationState.stage;
-  delete staticConfigurationState.searchUrl;
-  return objectHash(staticConfigurationState);
+  return objectHash(configurationState.filter);
 };
 
-const configurationDefaultState: configurationStateType = {
+const defaultConfiguration: Configuration = {
   stage: 0,
   loaded: false,
-  floors: AllFloors.slice(),
-  postcodes: [],
-  onlyOldBuilding: false,
-  onlyUnfurnished: true,
-  hasWBS: false,
-  mustHaveBalcony: false,
-  mustHaveKitchenette: false,
-  noKitchenette: false,
+  filter: {
+    floors: AllFloors.slice(),
+    postcodes: [],
+    onlyOldBuilding: false,
+    onlyUnfurnished: true,
+    hasWBS: false,
+    mustHaveBalcony: false,
+    mustHaveKitchenette: false,
+    noKitchenette: false
+  },
   applicationText: `${APPLICATION_TEMPLATES.SALUTATION},\n`,
   contactData: {
     salutation: SALUTATIONS.FRAU,
@@ -164,52 +166,46 @@ const configurationDefaultState: configurationStateType = {
 };
 
 export default function configuration(
-  state: configurationStateType = configurationDefaultState,
+  state: Configuration = defaultConfiguration,
   action: Action
-): configurationStateType {
+): Configuration {
   if (action.type === TOGGLE_POSTCODE) {
     const { postcode } = action.payload;
-    const { postcodes } = state;
+    const { postcodes } = state.filter;
 
-    if (postcodes.includes(postcode)) {
-      return Object.assign({}, state, {
-        postcodes: postcodes.filter(z => z !== postcode)
-      });
+    const postcodeIndex = postcodes.indexOf(postcode);
+    if (postcodeIndex !== -1) {
+      return dotProp.delete(state, `filter.postcodes.${postcodeIndex}`);
     }
 
-    return Object.assign({}, state, {
-      postcodes: postcodes.concat([postcode])
-    });
+    return dotProp.merge(state, 'filter.postcodes', [postcode]);
   }
 
   if (action.type === TOGGLE_FLOOR) {
     const { floor } = action.payload;
-    const { floors } = state;
+    const { floors } = state.filter;
 
-    if (floors.includes(floor)) {
-      return Object.assign({}, state, {
-        floors: floors.filter(z => z !== floor)
-      });
+    const floorIndex = floors.indexOf(floor);
+    if (floorIndex !== -1) {
+      return dotProp.delete(state, `filter.floors.${floorIndex}`);
     }
 
-    return Object.assign({}, state, {
-      floors: floors.concat([floor])
-    });
+    return dotProp.merge(state, 'filter.floors', [floor]);
   }
 
   switch (action.type) {
     case SET_CONFIGURATION:
       return action.payload.configuration;
     case RESET_CONFIGURATION:
-      return configurationDefaultState;
+      return defaultConfiguration;
     case NEXT_STAGE:
-      return Object.assign({}, state, { stage: state.stage + 1 });
+      return dotProp.set(state, 'stage', state.stage + 1);
     case PREVIOUS_STAGE:
-      return Object.assign({}, state, { stage: state.stage - 1 });
+      return dotProp.set(state, 'stage', state.stage - 1);
     case SET_SEARCH_URL:
-      return Object.assign({}, state, { searchUrl: action.payload.searchUrl });
+      return dotProp.set(state, 'searchUrl', action.payload.searchUrl);
     case RESET_POSTCODES:
-      return Object.assign({}, state, { postcodes: [] });
+      return dotProp.set(state, 'postcodes', []);
     case TOGGLE_BOOLEAN:
       return dotProp.toggle(state, action.payload.name);
     case SET_NUMBER:

app/actions/configuration.js
@@ -1,7 +1,7 @@
 import type {
   configurationBoolean,
   configurationNumbers,
-  configurationStateType
+  Configuration
 } from '../reducers/configuration';
 import type { Action } from '../reducers/types';
 import {
@@ -18,9 +18,7 @@ import {
   SET_STRING
 } from '../constants/actionTypes';
 
-export function setConfiguration(
-  configuration: configurationStateType
-): Action {
+export function setConfiguration(configuration: Configuration): Action {
   return {
     type: SET_CONFIGURATION,
     payload: { configuration }

app/middleware/configuration.js
@@ -4,16 +4,17 @@ import { setSearchUrl } from '../actions/configuration';
 import { refreshVerdicts } from '../actions/data';
 import type { Action, Dispatch, Store } from '../reducers/types';
 import { SET_SEARCH_URL } from '../constants/actionTypes';
-import type { configurationStateType } from '../reducers/configuration';
+import type { Configuration } from '../reducers/configuration';
 import districts from '../map/districts';
 import { numberToGermanFloatString } from '../utils/germanStrings';
+import { getConfigurationFilterHash } from '../reducers/configuration';
 
-function generateSearchUrl(configuration: configurationStateType): string {
+function generateSearchUrl(configuration: Configuration): string {
   let searchUrl =
     'https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin/';
   const overlappingDistricts = districts.filter(district =>
     district.postcodes.some(postcode =>
-      configuration.postcodes.includes(postcode)
+      configuration.filter.postcodes.includes(postcode)
     )
   );
 
@@ -36,19 +37,32 @@ function generateSearchUrl(configuration: configurationStateType): string {
   }
 
   searchUrl += `/${numberToGermanFloatString(
-    configuration.minimumRooms
+    configuration.filter.minimumRooms
   )}-${numberToGermanFloatString(
-    configuration.maximumRooms
+    configuration.filter.maximumRooms
   )}/${numberToGermanFloatString(
-    configuration.minimumArea
-  )}-/EURO--${numberToGermanFloatString(configuration.maximumRent)}`;
+    configuration.filter.minimumArea
+  )}-/EURO--${numberToGermanFloatString(configuration.filter.maximumRent)}`;
 
   return searchUrl;
 }
 
 // eslint-disable-next-line no-unused-vars
 export default (store: Store) => (next: Dispatch) => (action: Action) => {
-  if (action.meta && action.meta.configuration) {
+  if (!action.meta || !action.meta.configuration) {
+    return next(action);
+  }
+
+  const filterBeforeUpdate = getConfigurationFilterHash(
+    store.getState().configuration
+  );
+
+  const result = next(action);
+
+  if (
+    getConfigurationFilterHash(store.getState().configuration) !==
+    filterBeforeUpdate
+  ) {
     store.dispatch(refreshVerdicts());
 
     if (action.type !== SET_SEARCH_URL) {
@@ -60,5 +74,5 @@ export default (store: Store) => (next: Dispatch) => (action: Action) => {
     }
   }
 
-  return next(action);
+  return result;
 };

app/actions/bot.js
@@ -19,7 +19,7 @@ import { MAIN } from '../constants/targets';
 import applicationTextBuilder from '../flat/applicationTextBuilder';
 import { markCompleted } from './cache';
 import { CACHE_NAMES } from '../reducers/cache';
-import type { configurationStateType } from '../reducers/configuration';
+import type { Configuration } from '../reducers/configuration';
 import type { dataStateType } from '../reducers/data';
 import {
   fillForm,
@@ -138,7 +138,7 @@ export const generateApplicationTextAndSubmit = targetedAction<string>(
       configuration,
       data
     }: {
-      configuration: configurationStateType,
+      configuration: Configuration,
       data: dataStateType
     } = getState();
     const flatOverview = data.overview[flatId];

app/components/Configuration.js
@@ -6,7 +6,7 @@ import type {
   configurationBoolean,
   configurationNumbers
 } from '../reducers/configuration';
-import { type configurationStateType } from '../reducers/configuration';
+import { type Configuration as ConfigurationType } from '../reducers/configuration';
 import stages from './configurationStages';
 import type {
   FlexibleNode,
@@ -23,7 +23,7 @@ type Props = {
   toggleBoolean: (name: configurationBoolean) => void,
   setNumber: (name: configurationNumbers, value: ?number) => void,
   setString: (name: string, value: ?string) => void,
-  configuration: configurationStateType
+  configuration: ConfigurationType
 };
 
 export default class Configuration extends Component<Props> {

app/components/configurationStages/areaSelection.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../Configuration.scss';
 import PostcodeMap from '../PostcodeMap';
-import type { configurationStateType } from '../../reducers/configuration';
+import type { Configuration } from '../../reducers/configuration';
 import type { InheritedProps, StageDescription } from './types';
 
 const areaSelectionStage: StageDescription = {
@@ -20,7 +20,9 @@ const areaSelectionStage: StageDescription = {
   body: ({
     togglePostcode,
     resetPostcodes,
-    configuration: { postcodes }
+    configuration: {
+      filter: { postcodes }
+    }
   }: InheritedProps) => (
     <>
       <PostcodeMap
@@ -50,8 +52,8 @@ const areaSelectionStage: StageDescription = {
   buttons: {
     forward: {
       text: `Weiter`,
-      checkInvalid: (configuration: configurationStateType) => {
-        if (configuration.postcodes.length === 0) {
+      checkInvalid: (configuration: Configuration) => {
+        if (configuration.filter.postcodes.length === 0) {
           return 'Wähle mindestens einen Bezirk aus';
         }
 

app/components/configurationStages/flatDescription.js
@@ -6,7 +6,7 @@ import {
   MOVE_IN_WHO
 } from '../../reducers/configuration';
 import { floorToName } from '../../utils/germanStrings';
-import type { configurationStateType } from '../../reducers/configuration';
+import type { Configuration } from '../../reducers/configuration';
 import type { InheritedProps, StageDescription } from './types';
 import Disclaimer from './disclaimer';
 import TextField from '../inputs/TextInput';
@@ -26,17 +26,19 @@ const flatDescriptionStage: StageDescription = {
   ),
   body: ({
     configuration: {
-      floors,
-      onlyOldBuilding,
-      onlyUnfurnished,
-      hasWBS,
-      mustHaveBalcony,
-      mustHaveKitchenette,
-      noKitchenette,
-      maximumRent,
-      minimumArea,
-      minimumRooms,
-      maximumRooms,
+      filter: {
+        floors,
+        onlyOldBuilding,
+        onlyUnfurnished,
+        hasWBS,
+        mustHaveBalcony,
+        mustHaveKitchenette,
+        noKitchenette,
+        maximumRent,
+        minimumArea,
+        minimumRooms,
+        maximumRooms
+      },
       additionalInformation: { moveInWho, animals, moveInWhen }
     },
     toggleFloor,
@@ -52,7 +54,7 @@ const flatDescriptionStage: StageDescription = {
             Bis zu{' '}
             <NumberField
               value={maximumRent}
-              onChange={value => setNumber('maximumRent', value)}
+              onChange={value => setNumber('filter.maximumRent', value)}
             />
             € Kaltmiete
           </div>
@@ -60,20 +62,20 @@ const flatDescriptionStage: StageDescription = {
             Mindestens
             <NumberField
               value={minimumArea}
-              onChange={value => setNumber('minimumArea', value)}
+              onChange={value => setNumber('filter.minimumArea', value)}
             />
             m²
           </div>
           <div className={styles.searchParameter}>
             <NumberField
               value={minimumRooms}
-              onChange={value => setNumber('minimumRooms', value)}
+              onChange={value => setNumber('filter.minimumRooms', value)}
               step={0.5}
             />
             bis
             <NumberField
               value={maximumRooms}
-              onChange={value => setNumber('maximumRooms', value)}
+              onChange={value => setNumber('filter.maximumRooms', value)}
               step={0.5}
             />{' '}
             Zimmer
@@ -82,13 +84,13 @@ const flatDescriptionStage: StageDescription = {
           <input
             type="checkbox"
             checked={hasWBS}
-            onChange={() => toggleBoolean('hasWBS')}
+            onChange={() => toggleBoolean('filter.hasWBS')}
           />{' '}
           Ja &nbsp;&nbsp;
           <input
             type="checkbox"
             checked={!hasWBS}
-            onChange={() => toggleBoolean('hasWBS')}
+            onChange={() => toggleBoolean('filter.hasWBS')}
           />{' '}
           Nein
           <div style={{ marginTop: '0.5em', lineHeight: 0.9 }}>
@@ -102,7 +104,7 @@ const flatDescriptionStage: StageDescription = {
             <input
               type="checkbox"
               checked={onlyOldBuilding}
-              onChange={() => toggleBoolean('onlyOldBuilding')}
+              onChange={() => toggleBoolean('filter.onlyOldBuilding')}
             />{' '}
             Unbedingt Altbau (vor 1950 errichtet)
           </div>
@@ -110,7 +112,7 @@ const flatDescriptionStage: StageDescription = {
             <input
               type="checkbox"
               checked={mustHaveBalcony}
-              onChange={() => toggleBoolean('mustHaveBalcony')}
+              onChange={() => toggleBoolean('filter.mustHaveBalcony')}
             />{' '}
             Unbedingt mit Balkon / Terasse
           </div>
@@ -118,7 +120,7 @@ const flatDescriptionStage: StageDescription = {
             <input
               type="checkbox"
               checked={mustHaveKitchenette}
-              onChange={() => toggleBoolean('mustHaveKitchenette')}
+              onChange={() => toggleBoolean('filter.mustHaveKitchenette')}
             />{' '}
             Unbedingt <em>mit</em> Einbauküche
           </div>
@@ -126,7 +128,7 @@ const flatDescriptionStage: StageDescription = {
             <input
               type="checkbox"
               checked={noKitchenette}
-              onChange={() => toggleBoolean('noKitchenette')}
+              onChange={() => toggleBoolean('filter.noKitchenette')}
             />{' '}
             Unbedingt <em>ohne</em> Einbauküche
           </div>
@@ -134,7 +136,7 @@ const flatDescriptionStage: StageDescription = {
             <input
               type="checkbox"
               checked={onlyUnfurnished}
-              onChange={() => toggleBoolean('onlyUnfurnished')}
+              onChange={() => toggleBoolean('filter.onlyUnfurnished')}
             />{' '}
             Unbedingt unmöbliert
           </div>
@@ -202,8 +204,8 @@ const flatDescriptionStage: StageDescription = {
   buttons: {
     forward: {
       text: `Weiter`,
-      checkInvalid: (configuration: configurationStateType) => {
-        if (configuration.floors.length === 0) {
+      checkInvalid: (configuration: Configuration) => {
+        if (configuration.filter.floors.length === 0) {
           return 'Wähle mindestens ein Stockwerk aus';
         }
 

app/components/configurationStages/personalData.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../Configuration.scss';
 import { EMPLOYMENT_STATUS, SALUTATIONS } from '../../reducers/configuration';
-import type { configurationStateType } from '../../reducers/configuration';
+import type { Configuration } from '../../reducers/configuration';
 import type { InheritedProps, StageDescription } from './types';
 import Disclaimer from './disclaimer';
 import TextField from '../inputs/TextInput';
@@ -204,7 +204,7 @@ const personalDataStage: StageDescription = {
   buttons: {
     forward: {
       text: `Weiter`,
-      checkInvalid: (configuration: configurationStateType) => {
+      checkInvalid: (configuration: Configuration) => {
         const checks = {
           contactData: {
             firstName: 'deinen Vornamen',

commit 3d800811a95d95741b927390defddbbc51e18ef7

Fix: resetting postcodes (regression introduced in 6f671e9b0d14b7160c52375c8c5430f1af38f868)

app/reducers/configuration.js
@@ -235,7 +235,7 @@ export default function configuration(
     case SET_SEARCH_URL:
       return dotProp.set(state, 'searchUrl', action.payload.searchUrl);
     case RESET_POSTCODES:
-      return dotProp.set(state, 'postcodes', []);
+      return dotProp.set(state, 'filter.postcodes', []);
     case TOGGLE_BOOLEAN:
       return dotProp.toggle(state, action.payload.name);
     case SET_NUMBER: