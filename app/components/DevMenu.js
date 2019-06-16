754f9508df21d0523bbda2b82ff3bd9fa78f8190
neopostmodern <clemens@neopostmodern.com>
2019-06-16 07:08:10 +0200

    Make DevMenu collapsible

--- app/components/DevMenu.js
+++ app/components/DevMenu.js
@@ -3,21 +3,28 @@ import React, { Component } from 'react';
 import styles from './DevMenu.scss';
 import type { BrowserViewName, Views } from '../reducers/electron';
 
-type Props = {
+type DevMenuProps = {
   views: Views,
   showDevTools: (name: BrowserViewName) => void,
   resetConfiguration: () => void,
   resetCache: () => void
 };
 
-export default class BotOverlay extends Component<Props> {
-  props: Props;
+type DevMenuState = {
+  expanded: boolean
+};
+
+export default class DevMenu extends Component<DevMenuProps, DevMenuState> {
+  props: DevMenuProps;
+
+  state: DevMenuState = { expanded: false };
 
   render() {
     const { views, showDevTools, resetConfiguration, resetCache } = this.props;
+    const { expanded } = this.state;
 
     return (
-      <div className={styles.container}>
+      <div className={`${styles.container} ${expanded ? '' : styles.collapsed}`}>
         <div className={styles.menu}>
           {Object.keys(views).map(viewName => (
             <div key={viewName} className={styles.menuItem}>
@@ -36,6 +43,18 @@ export default class BotOverlay extends Component<Props> {
           <div className={styles.menuItem}>
             <button type="button" onClick={resetConfiguration}>
               Reset configuration <span className="material-icons">replay</span>
+            </button>
+          </div>
+          <div className={styles.menuItem} id={styles.visibilityToggle}>
+            <button
+              type="button"
+              onClick={() =>
+                this.setState(prevState => ({ expanded: !prevState.expanded }))
+              }
+            >
+              <span className="material-icons">
+                {expanded ? 'chevron_right' : 'chevron_left'}
+              </span>
             </button>
           </div>
         </div>
