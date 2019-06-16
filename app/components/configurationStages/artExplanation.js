0a1c95777251bfc5de955515e39718ad0a6de1e0
neopostmodern <clemens@neopostmodern.com>
2019-06-16 07:52:19 +0200

    Add disclaimer about housing situation in Berlin Add brief mention of automation speculation

--- app/components/configurationStages/artExplanation.js
+++ app/components/configurationStages/artExplanation.js
@@ -4,6 +4,9 @@ import type { InheritedProps, StageDescription } from './types';
 import type { Configuration } from '../../reducers/configuration';
 
 const configurationExplanationStage: StageDescription = {
+  container: {
+    className: styles.high
+  },
   title: '',
   body: ({
     configuration: {
@@ -33,6 +36,20 @@ const configurationExplanationStage: StageDescription = {
           auf GitHub
         </a>{' '}
         an), vielleicht auch nicht.
+        <br />
+        <br />
+        Außerdem muss natürlich klar sein, dass der Bot dir nicht magisch zu
+        einer Wohnung verhelfen kann:
+        <br />
+        Er behebt weder den Wohnungsmangel, noch dass eventuell Vermieter_innen
+        und Hausverwaltungen dich bei der Auswahl diskriminieren, noch die
+        vielen anderen Probleme, die es zur Zeit mit dem Wohnungsmarkt in Berlin
+        gibt.
+        <br />
+        <br />
+        Aber er kann uns zeigen, dass eine andere Form der Automatisierung
+        möglich wäre, oder zumindest spekulieren und fragen, wie wir uns diese
+        vorstellen und wünschen würden.
         <br />
         <br />
         Bitte bestätige also folgendes:
