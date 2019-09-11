neopostmodern/wohnungsbot

commit 693f40bd2c920d51e7e289158401a4848f1b9072

Fix: correct overlay for unsuitable flats

app/components/BotOverlay.js
@@ -18,6 +18,7 @@ type Props = {
   botMessage: string,
   showOverlay: boolean,
   alreadyAppliedFlatIds: Array<string>,
+  unsuitableFlatIds: Array<string>,
   performScroll: (name: 'puppet', deltaY: number) => void
 };
 
@@ -105,7 +106,8 @@ export default class BotOverlay extends Component<Props> {
       verdicts,
       isBotActing,
       showOverlay,
-      alreadyAppliedFlatIds
+      alreadyAppliedFlatIds,
+      unsuitableFlatIds
     } = this.props;
 
     return (
@@ -136,6 +138,7 @@ export default class BotOverlay extends Component<Props> {
                     flatId={flatId}
                     verdict={verdicts[flatId]}
                     isAlreadyApplied={alreadyAppliedFlatIds.includes(flatId)}
+                    isUnsuitable={unsuitableFlatIds.includes(flatId)}
                   />
                 </div>
               )

app/components/util/Verdict.js
@@ -6,21 +6,63 @@ import type { Verdict } from '../../reducers/data';
 type VerdictProps = {
   flatId: string,
   verdict: Verdict,
-  isAlreadyApplied: boolean
+  isAlreadyApplied: boolean,
+  isUnsuitable: boolean
+};
+
+const VerdictSummary = ({
+  verdict,
+  isAlreadyApplied,
+  isUnsuitable
+}: {
+  verdict: Verdict,
+  isAlreadyApplied: boolean,
+  isUnsuitable: boolean
+}) => {
+  if (isAlreadyApplied) {
+    return <div className={styles.oneLineReason}>Bewerbung abgeschickt</div>;
+  }
+
+  if (isUnsuitable) {
+    return <div className={styles.oneLineReason}>Unpassend</div>;
+  }
+
+  return (
+    <>
+      {verdict.reasons.map(({ reason, result }) => (
+        <div key={reason} className={styles.reason}>
+          <div className={styles.reasonIcon}>
+            <span
+              className={`material-icons standalone-icon ${
+                result ? 'good' : 'bad'
+              }`}
+            >
+              {result ? 'check' : 'block'}
+            </span>
+          </div>
+          <div>{reason}</div>
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default class VerdictComponent extends React.Component<VerdictProps> {
   getMainIcon(): string {
-    const { verdict, isAlreadyApplied } = this.props;
+    const { verdict, isAlreadyApplied, isUnsuitable } = this.props;
     if (isAlreadyApplied) {
       return 'done_outline';
+    }
+
+    if (isUnsuitable) {
+      return 'block';
     }
 
     return verdict.result ? 'thumb_up_alt' : 'thumb_down_alt';
   }
 
   render() {
-    const { verdict, isAlreadyApplied, flatId } = this.props;
+    const { verdict, isAlreadyApplied, isUnsuitable, flatId } = this.props;
 
     return (
       <div className={styles.verdictOverlay}>
@@ -29,33 +71,20 @@ export default class VerdictComponent extends React.Component<VerdictProps> {
             <div className={styles.summary}>
               <span
                 className={`material-icons standalone-icon ${
-                  verdict.result || isAlreadyApplied ? 'good' : 'bad'
+                  !isUnsuitable && (verdict.result || isAlreadyApplied)
+                    ? 'good'
+                    : 'bad'
                 }`}
               >
                 {this.getMainIcon()}
               </span>
             </div>
             <div>
-              {isAlreadyApplied ? (
-                <div className={styles.oneLineReason}>
-                  Bewerbung abgeschickt
-                </div>
-              ) : (
-                verdict.reasons.map(({ reason, result }) => (
-                  <div key={reason} className={styles.reason}>
-                    <div className={styles.reasonIcon}>
-                      <span
-                        className={`material-icons standalone-icon ${
-                          result ? 'good' : 'bad'
-                        }`}
-                      >
-                        {result ? 'check' : 'block'}
-                      </span>
-                    </div>
-                    <div>{reason}</div>
-                  </div>
-                ))
-              )}
+              <VerdictSummary
+                verdict={verdict}
+                isAlreadyApplied={isAlreadyApplied}
+                isUnsuitable={isUnsuitable}
+              />
             </div>
           </>
         ) : (

app/containers/BotOverlayPage.js
@@ -17,7 +17,12 @@ function mapStateToProps(state: stateType) {
     isBotActing: state.bot.isActive,
     botMessage: state.bot.message,
     showOverlay: state.bot.showOverlay,
-    alreadyAppliedFlatIds: Object.keys(state.cache.applications)
+    alreadyAppliedFlatIds: Object.entries(state.cache.applications)
+      .filter(([flatId, application]) => application.reason !== 'UNSUITABLE')
+      .map(([flatId]) => flatId),
+    unsuitableFlatIds: Object.entries(state.cache.applications)
+      .filter(([flatId, application]) => application.reason === 'UNSUITABLE')
+      .map(([flatId]) => flatId)
   };
 }
 

commit 26d6136bb65f2a964ad0a1d41219fc7e448863df

Fix: take unsuitable flats into account for speech bubble (regression from 693f40bd2c920d51e7e289158401a4848f1b9072)

app/components/BotOverlay.js
@@ -72,7 +72,8 @@ export default class BotOverlay extends Component<Props> {
       isPuppetLoading,
       botMessage,
       verdicts,
-      alreadyAppliedFlatIds
+      alreadyAppliedFlatIds,
+      unsuitableFlatIds
     } = this.props;
 
     if (isPuppetLoading) {
@@ -85,18 +86,22 @@ export default class BotOverlay extends Component<Props> {
 
     // eslint-disable-next-line flowtype/no-weak-types
     const matchedFlats = ((Object.values(verdicts): any): Array<Verdict>)
-      .filter(({ result }) => result)
+      .filter(
+        ({ flatId, result }) => result && !unsuitableFlatIds.includes(flatId)
+      )
       .map(({ flatId }) => flatId);
 
     const notAppliedYetFlats = matchedFlats.filter(
-      flatId => !alreadyAppliedFlatIds.includes(flatId)
+      flatId =>
+        !alreadyAppliedFlatIds.includes(flatId) &&
+        !unsuitableFlatIds.includes(flatId)
     );
 
     if (matchedFlats.length > 0) {
       return `${notAppliedYetFlats.length} neue passende Wohnungen gefunden (${matchedFlats.length} insgesamt).`;
     }
 
-    return 'Ich bin bereit, aber es gibt keine Wohnungen!';
+    return 'Ich bin bereit, aber es gibt keine passenden Wohnungen!';
   }
 
   render() {