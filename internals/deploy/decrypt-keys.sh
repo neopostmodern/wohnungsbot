0e8610e5b6fc3f0b567308444238597c123d56ab
neopostmodern <clemens@neopostmodern.com>
2021-01-26 13:15:41 +0100

    ci: encrypted keys
    - add encrypted key file
    - encrypt/decrypt scripts
    - decryption step in action

---
+++ internals/deploy/decrypt-keys.sh
@@ -0,0 +1,5 @@
+keysFile="${BASH_SOURCE%/*}/../../app/constants/keys.json"
+gpg --quiet --batch --yes --decrypt \
+  --passphrase="${KEYS_DECRYPT_PASSPHRASE}" \
+  --output "${keysFile}" \
+   "${keysFile}.gpg"
