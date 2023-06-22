01c52dc87e12df5e438837c8d973fab1cf87830c
Georg Hoelger <georghoelger@gmx.de>
2023-06-22 11:34:24 +0200

    feat: implement basic manual login

--- app/middleware/login.js
+++ app/middleware/login.js
@@ -3,12 +3,12 @@
 import { LOGIN, LOGOUT } from '../constants/actionTypes';
 import type { Action, Store } from '../reducers/types';
 import { sleep, timeout } from '../utils/async';
-import performLogin from '../utils/performLogin';
+import { performLogin, performManualLogin } from '../utils/performLogin';
 import { electronRouting } from '../actions/electron';
 import { abortable } from '../utils/generators';
 import AbortionSystem, { ABORTION_ERROR } from '../utils/abortionSystem';
 import ElectronUtilsRedux from '../utils/electronUtilsRedux';
-import { LOGINSTATUS } from '../reducers/configuration';
+import { LOGINSTATUS, USEACCOUNT } from '../reducers/configuration';
 import { logout, setBotIsActing, setLoginStatus } from '../actions/bot';
 import performLogout from '../utils/performLogout';
 import { electronObjects } from '../store/electronObjects';
@@ -23,7 +23,7 @@ export default (store: Store) =>
         }
       } = store.getState();
 
-      if (useAccount) {
+      if (useAccount == USEACCOUNT.JA) {
         await store.dispatch(setBotIsActing(false));
         await store.dispatch(
           electronRouting('puppet', 'https://www.immobilienscout24.de/')
@@ -40,6 +40,36 @@ export default (store: Store) =>
         try {
           await timeout(
             abortablePerformLogin(
+              store.dispatch,
+              electronUtils,
+              store.getState().configuration
+            ),
+            300000
+          );
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.error(`Error during login: ${error}`);
+          AbortionSystem.abort(ABORTION_ERROR);
+          await store.dispatch(setLoginStatus(LOGINSTATUS.ERROR));
+        }
+        await sleep(2000);
+      } else if (useAccount == USEACCOUNT.MANUELL) {
+        await store.dispatch(setBotIsActing(false));
+        await store.dispatch(
+          electronRouting('puppet', 'https://www.immobilienscout24.de/')
+        );
+        await sleep(4000);
+        const { webContents } = electronObjects.views.puppet;
+        const electronUtils = new ElectronUtilsRedux(
+          webContents,
+          store.dispatch
+        );
+        const { abortableAction: abortablePerformManualLogin, abort } =
+          abortable(performManualLogin);
+        AbortionSystem.registerAbort(abort);
+        try {
+          await timeout(
+            abortablePerformManualLogin(
               store.dispatch,
               electronUtils,
               store.getState().configuration
