08f5f1dd52350a26de8de0207eab8c173850370c
Georg Hoelger <georghoelger@gmx.de>
2023-09-18 18:49:06 +0200

    fix: update selector for entry title

--- app/utils/selectors.js
+++ app/utils/selectors.js
@@ -3,4 +3,4 @@
 export const entrySelector = (entryId: string): string =>
   `[data-id="${entryId}"]`;
 export const entryTitleSelector = (entryId: string): string =>
-  `[data-go-to-expose-id="${entryId}"]`;
+  `[data-go-to-expose-id="${entryId}"] .result-list-entry__brand-title`;
