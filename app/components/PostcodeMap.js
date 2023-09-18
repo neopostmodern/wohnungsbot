neopostmodern/wohnungsbot

commit 3d29c97f11d64e92748b765e461756f6d8bf1d81

deps: remove react-hot-loader

babel.config.js
@@ -2,7 +2,7 @@
 
 const developmentEnvironments = ['development', 'test'];
 
-const developmentPlugins = [require('react-hot-loader/babel')];
+const developmentPlugins = [];
 
 const productionPlugins = [
   require('babel-plugin-dev-expression'),

app/index.js
@@ -1,6 +1,5 @@
 import React, { Fragment } from 'react';
 import { render } from 'react-dom';
-import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 import { RENDERER } from './constants/targets';
 import Root from './containers/Root';
 import getHistory from './store/history';
@@ -16,10 +15,6 @@ import('./store/configureStore')
   .then((store) => {
     const history = getHistory();
 
-    const AppContainer = process.env.PLAIN_HMR
-      ? Fragment
-      : ReactHotAppContainer;
-
     render(
       <AppContainer>
         <Root store={store} history={history} />
@@ -32,9 +27,9 @@ import('./store/configureStore')
         // eslint-disable-next-line global-require
         const NextRoot = require('./containers/Root').default;
         render(
-          <AppContainer>
+          <Fragment>
             <NextRoot store={store} history={history} />
-          </AppContainer>,
+          </Fragment>,
           document.getElementById('root')
         );
       });

configs/webpack.config.renderer.dev.babel.js
@@ -48,7 +48,6 @@ export default merge(baseConfig, {
   target: 'electron-renderer',
 
   entry: [
-    ...(process.env.PLAIN_HMR ? [] : ['react-hot-loader/patch']),
     `webpack-dev-server/client?http://localhost:${port}/`,
     'webpack/hot/only-dev-server',
     require.resolve('../app/index')

commit 72798406216af3d694c8acccf318295d030be542

build: updated dependencies, migrate configs to TypeScript (PR 156), closes #151
- no implicit default export destructuring  
- remove outdated and offending packages, sometimes without replacement for now:
  - styleling-config-prettier
  - react-hot-loader
  - chalk

* deps: pin the volta npm version

* deps: execute `npx npm-check-updates -u` and delete lock file

* deps: remove sylelint-config-prettier

* deps: remove react-hot-loader

* deps: rename webpack.config.base.js -> *.babel.js

* deps: remove chalk

* deps: add "@babel/preset-typescript"

* deps: add package-lock.json

* chore: update gitignore

* deps: "npm run-script build-main" runs through

* chore: remove obsolete build configs & scripts

* build: port all build configs and scripts to typescript

* build: no implicit destructuring of imports

* fix: remove obsolete `AppContainer` reference

- regression from 3d29c97f11d64e92748b765e461756f6d8bf1d81

* chore: adjust eslint config file extension

- regression from 56f27768bee582dcd907318777977754cd4e8525

* build: update `package-lock.json` after merge

* build: typescript fixes for build configs

---------

Co-authored-by: Georg Hoelger <georghoelger@gmx.de>

.gitignore
@@ -37,6 +37,7 @@ flow-typed/npm/*
 # App packaged
 release
 app/main.prod.js
+app/main.prod.js.LICENSE.txt
 app/main.prod.js.map
 app/renderer.prod.js
 app/renderer.prod.js.map

.stylelintrc
@@ -1,3 +1,3 @@
 {
-  "extends": ["stylelint-config-standard", "stylelint-config-prettier"]
+  "extends": ["stylelint-config-standard"]
 }

app/components/PostcodeMap.js
@@ -1,12 +1,10 @@
 // @flow
 import React from 'react';
-import {
-  MapContainer,
-  GeoJSON,
-  TileLayer,
-  Marker,
-  useMapEvent
-} from 'react-leaflet';
+import { MapContainer } from 'react-leaflet/MapContainer';
+import { GeoJSON } from 'react-leaflet/GeoJSON';
+import { TileLayer } from 'react-leaflet/TileLayer';
+import { Marker } from 'react-leaflet/Marker';
+import { useMapEvent } from 'react-leaflet/hooks';
 import { divIcon, type Layer } from 'leaflet';
 import { feature } from 'topojson';
 import topoData from '../map/berlin-postcodes-data.topo';

app/components/Sidebar.js
@@ -2,9 +2,11 @@
 import React, { Component } from 'react';
 import styles from './Sidebar.scss';
 import type { ApplicationData, BaseCacheEntry } from '../reducers/cache';
-import { homepage, version, bugs } from '../../package.json';
+import packageJson from '../../package.json';
 import RecentApplication from './sidebar/recentApplication';
 import { LOADING, UP_TO_DATE } from '../constants/updater';
+
+const { homepage, version, bugs } = packageJson;
 
 type Props = {
   showConfiguration: () => void,
@@ -24,10 +26,16 @@ export default class Sidebar extends Component<Props, State> {
   state: State = {};
 
   async componentWillMount() {
-    const response = await fetch('https://wohnungsbot.de/announcement.html');
-    const announcement = await response.text();
+    try {
+      const response = await fetch('https://wohnungsbot.de/announcement.html');
+      const announcement = await response.text();
 
-    this.setState({ announcement });
+      this.setState({ announcement });
+    } catch (error) {
+      this.setState({
+        announcement: `Neuigkeiten zum Wohnungsbot konnten leider nicht geladen werden.<br/><small>Technische Fehlermeldung:<pre>${error.toString()}</pre></small>`
+      });
+    }
   }
 
   render() {

app/index.jsx
@@ -1,6 +1,5 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 import { RENDERER } from './constants/targets';
 import Root from './containers/Root';
 import getHistory from './store/history';
@@ -16,14 +15,8 @@ import('./store/configureStore')
   .then((store) => {
     const history = getHistory();
 
-    const AppContainer = process.env.PLAIN_HMR
-      ? Fragment
-      : ReactHotAppContainer;
-
     render(
-      <AppContainer>
-        <Root store={store} history={history} />
-      </AppContainer>,
+      <Root store={store} history={history} />,
       document.getElementById('root')
     );
 
@@ -32,9 +25,7 @@ import('./store/configureStore')
         // eslint-disable-next-line global-require
         const NextRoot = require('./containers/Root').default;
         render(
-          <AppContainer>
-            <NextRoot store={store} history={history} />
-          </AppContainer>,
+          <NextRoot store={store} history={history} />,
           document.getElementById('root')
         );
       });

app/utils/email.js
@@ -1,9 +1,9 @@
 // @flow
 
 import sendgrid from '@sendgrid/mail';
-import { SENDGRID_API_KEY } from '../constants/keys';
+import keys from '../constants/keys';
 
-sendgrid.setApiKey(SENDGRID_API_KEY);
+sendgrid.setApiKey(keys.SENDGRID_API_KEY);
 
 const sendMail = async (to: string, subject: string, text: string) => {
   const message = {

babel.config.js
@@ -2,7 +2,7 @@
 
 const developmentEnvironments = ['development', 'test'];
 
-const developmentPlugins = [require('react-hot-loader/babel')];
+const developmentPlugins = [];
 
 const productionPlugins = [
   require('babel-plugin-dev-expression'),
@@ -24,14 +24,7 @@ module.exports = (api) => {
 
   return {
     presets: [
-      [
-        require('@babel/preset-env'),
-        {
-          targets,
-          useBuiltIns: 'usage',
-          corejs: 3
-        }
-      ],
+      [require('@babel/preset-env')],
       require('@babel/preset-flow'),
       [require('@babel/preset-react'), { development }]
     ],

configs/webpack.config.eslint.ts
@@ -1,4 +1,4 @@
 /* eslint import/no-unresolved: off, import/no-self-import: off */
 require('@babel/register');
 
-module.exports = require('./webpack.config.renderer.dev.babel').default;
+module.exports = require('./webpack.config.renderer.dev').default;

configs/webpack.config.renderer.dev.dll.ts
@@ -29,7 +29,7 @@ export default merge(baseConfig, {
   /**
    * Use `module` from `webpack.config.renderer.dev.js`
    */
-  module: require('./webpack.config.renderer.dev.babel').default.module,
+  module: require('./webpack.config.renderer.dev').default.module,
 
   entry: {
     renderer: Object.keys(dependencies || {})

configs/webpack.config.renderer.dev.ts
@@ -9,8 +9,7 @@
 
 import path from 'path';
 import fs from 'fs';
-import webpack from 'webpack';
-import chalk from 'chalk';
+import webpack, { Configuration } from 'webpack';
 import { merge } from 'webpack-merge';
 import { spawn, execSync } from 'child_process';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
@@ -33,14 +32,12 @@ const requiredByDLLConfig = module.parent.filename.includes(
  */
 if (!requiredByDLLConfig && !(fs.existsSync(dll) && fs.existsSync(manifest))) {
   console.log(
-    chalk.black.bgYellow.bold(
-      'The DLL files are missing. Sit back while we build them for you with "yarn build-dll"'
-    )
+    'The DLL files are missing. Sit back while we build them for you with "yarn build-dll"'
   );
   execSync('yarn build-dll');
 }
 
-export default merge(baseConfig, {
+export default merge<Configuration>(baseConfig, {
   devtool: 'inline-source-map',
 
   mode: 'development',
@@ -48,10 +45,9 @@ export default merge(baseConfig, {
   target: 'electron-renderer',
 
   entry: [
-    ...(process.env.PLAIN_HMR ? [] : ['react-hot-loader/patch']),
     `webpack-dev-server/client?http://localhost:${port}/`,
     'webpack/hot/only-dev-server',
-    require.resolve('../app/index')
+    require.resolve('../app/index.jsx')
   ],
 
   output: {

configs/webpack.config.renderer.prod.ts
@@ -3,7 +3,7 @@
  */
 
 import path from 'path';
-import webpack from 'webpack';
+import webpack, { Configuration } from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
@@ -14,14 +14,14 @@ import CheckNodeEnv from '../internals/scripts/CheckNodeEnv';
 import { styleRules, imageAndFontRules } from './commonRules';
 
 CheckNodeEnv('production');
-export default merge(baseConfig, {
+export default merge<Configuration>(baseConfig, {
   devtool: 'source-map',
 
   mode: 'production',
 
   target: 'electron-renderer',
 
-  entry: path.join(__dirname, '..', 'app/index'),
+  entry: path.join(__dirname, '..', 'app/index.jsx'),
 
   output: {
     path: path.join(__dirname, '..', 'app/dist'),

configs/webpack.config.web.prod.babel.js
@@ -1,115 +1,0 @@
-/**
- * Build config for an independent web distribution
- */
-
-import path from 'path';
-import webpack from 'webpack';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin';
-import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
-import merge from 'webpack-merge';
-import TerserPlugin from 'terser-webpack-plugin';
-import baseConfig from './webpack.config.base';
-import CheckNodeEnv from '../internals/scripts/CheckNodeEnv';
-import { styleRules, imageAndFontRules } from './commonRules';
-
-CheckNodeEnv('production');
-const mergedConfig = merge.smart(baseConfig, {
-  devtool: 'source-map',
-
-  mode: 'production',
-
-  target: 'web',
-
-  entry: path.join(__dirname, '..', 'app/index'),
-
-  output: {
-    path: path.join(__dirname, '..', 'app/web'),
-    publicPath: './',
-    filename: 'web.prod.js',
-    chunkFilename: '[name].bundle.js',
-    libraryTarget: 'var'
-  },
-
-  module: {
-    rules: [...styleRules, ...imageAndFontRules]
-  },
-
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        parallel: true
-      }),
-      new OptimizeCSSAssetsPlugin({
-        cssProcessorOptions: {
-          map: {
-            inline: false,
-            annotation: true
-          }
-        }
-      })
-    ],
-    splitChunks: {
-      chunks: 'async',
-      name: true
-    }
-  },
-
-  plugins: [
-    /**
-     * Create global constants which can be configured at compile time.
-     *
-     * Useful for allowing different behaviour between development builds and
-     * release builds
-     *
-     * NODE_ENV should be production so that modules do not perform certain
-     * development checks
-     */
-
-    new webpack.DefinePlugin({
-      __TARGET__: '"web"'
-    }),
-
-    // block everything with 'electron' in it (except in folder `actions` or
-    // `reducers`, which is just redux infrastructure) or 'persist'(ance)
-    new webpack.IgnorePlugin({
-      checkResource(resource: string) {
-        if (/electron/.test(resource)) {
-          return !/actions/.test(resource) && !/Utils/.test(resource);
-        }
-
-        if (/persist/.test(resource)) {
-          return true;
-        }
-
-        if (/middleware\/helpers/.test(resource)) {
-          return true;
-        }
-
-        return false;
-      },
-      checkContext(context: string) {
-        return !/reducers/.test(context);
-      }
-    }),
-
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'production'
-    }),
-
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
-    }),
-
-    new BundleAnalyzerPlugin({
-      analyzerMode:
-        process.env.OPEN_ANALYZER === 'true' ? 'server' : 'disabled',
-      openAnalyzer: process.env.OPEN_ANALYZER === 'true'
-    })
-  ]
-});
-
-// override
-mergedConfig.externals = {};
-
-export default mergedConfig;

internals/scripts/CheckBuiltsExist.js
@@ -1,35 +1,0 @@
-// @flow
-// Check if the renderer and main bundles are built
-import path from 'path';
-import chalk from 'chalk';
-import fs from 'fs';
-
-function CheckBuildsExist() {
-  const mainPath = path.join(__dirname, '..', '..', 'app', 'main.prod.js');
-  const rendererPath = path.join(
-    __dirname,
-    '..',
-    '..',
-    'app',
-    'dist',
-    'renderer.prod.js'
-  );
-
-  if (!fs.existsSync(mainPath)) {
-    throw new Error(
-      chalk.whiteBright.bgRed.bold(
-        'The main process is not built yet. Build it by running "yarn build-main"'
-      )
-    );
-  }
-
-  if (!fs.existsSync(rendererPath)) {
-    throw new Error(
-      chalk.whiteBright.bgRed.bold(
-        'The renderer process is not built yet. Build it by running "yarn build-renderer"'
-      )
-    );
-  }
-}
-
-CheckBuildsExist();

internals/scripts/CheckNodeEnv.ts
@@ -1,6 +1,4 @@
 // @flow
-import chalk from 'chalk';
-
 export default function CheckNodeEnv(expectedEnv: string) {
   if (!expectedEnv) {
     throw new Error('"expectedEnv" not set');
@@ -8,9 +6,7 @@ export default function CheckNodeEnv(expectedEnv: string) {
 
   if (process.env.NODE_ENV !== expectedEnv) {
     console.log(
-      chalk.whiteBright.bgRed.bold(
-        `"process.env.NODE_ENV" must be "${expectedEnv}" to use this webpack config`
-      )
+      `"process.env.NODE_ENV" must be "${expectedEnv}" to use this webpack config`
     );
     process.exit(2);
   }

internals/scripts/CheckPortInUse.js
@@ -1,19 +1,0 @@
-// @flow
-import chalk from 'chalk';
-import detectPort from 'detect-port';
-
-(function CheckPortInUse() {
-  const port: string = process.env.PORT || '1212';
-
-  detectPort(port, (err: ?Error, availablePort: number) => {
-    if (port !== String(availablePort)) {
-      throw new Error(
-        chalk.whiteBright.bgRed.bold(
-          `Port "${port}" on "localhost" is already in use. Please use another port. ex: PORT=4343 yarn dev`
-        )
-      );
-    } else {
-      process.exit(0);
-    }
-  });
-})();

internals/scripts/CheckPortInUse.ts
@@ -1,0 +1,16 @@
+// @flow
+import detectPort from 'detect-port';
+
+(function CheckPortInUse() {
+  const port: number = parseInt(process.env.PORT || '1212', 10);
+
+  detectPort(port, (err: Error, availablePort: number) => {
+    if (port !== availablePort) {
+      throw new Error(
+        `Port "${port}" on "localhost" is already in use. Please use another port. ex: PORT=4343 yarn dev`
+      );
+    } else {
+      process.exit(0);
+    }
+  });
+})();