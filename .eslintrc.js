e1f7450eece8f7154ef02e3654caaf35f76d8b15
Georg Hoelger <georghoelger@gmx.de>
2023-11-17 10:56:22 +0100

    refactor: ran lint --fix
    NODE_ENV=development npx eslint . --ext .js,.jsx,.ts,.tsx --fix
    
    (remaining: 159 errors, 12 warnings)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,30 +3,30 @@ module.exports = {
   extends: 'erb',
   plugins: ['@typescript-eslint'],
   rules: {
-    "no-console": "warn",
-    "no-await-in-loop": "off",
-    "react/static-property-placement": "off",
-    "react/state-in-constructor": "off",
-    "import/extensions": "off",
-    "import/no-import-module-exports": "off",
-    "import/no-extraneous-dependencies": "off",
-    "react/jsx-filename-extension": [2, { "extensions": [".tsx"] }],
+    'no-console': 'warn',
+    'no-await-in-loop': 'off',
+    'react/static-property-placement': 'off',
+    'react/state-in-constructor': 'off',
+    'import/extensions': 'off',
+    'import/no-import-module-exports': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx'] }]
   },
   parserOptions: {
     ecmaVersion: 2022,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   settings: {
     'import/resolver': {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {},
       webpack: {
-        config: require.resolve('./configs/webpack.config.eslint.ts'),
+        config: require.resolve('./configs/webpack.config.eslint.ts')
       },
-      typescript: {},
+      typescript: {}
     },
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx'],
-    },
-  },
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    }
+  }
 };
