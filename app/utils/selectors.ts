939fd0ac37ccb606fbb0641bfd2592a33ccba738
neopostmodern <clemens@neopostmodern.com>
2024-03-22 11:21:31 +0100

    fix: update selector for entry title in overview, fixes #195

--- app/utils/selectors.ts
+++ app/utils/selectors.ts
@@ -1,4 +1,4 @@
 export const entrySelector = (entryId: string): string =>
   `[data-id="${entryId}"]`;
 export const entryTitleSelector = (entryId: string): string =>
-  `[data-go-to-expose-id="${entryId}"] > h2`;
+  `.result-list-entry__data a[data-exp-id="${entryId}"] > h2`;
