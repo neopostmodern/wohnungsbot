d54cfd0af14553c6064930d438e8ca95c881f7c9
neopostmodern <clemens@neopostmodern.com>
2024-10-21 15:14:44 +0200

    feat: add react and scss (as modules)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import styles from './App.module.scss'
+
+const App = () => {
+    return <div className={styles.wrapper}>
+        <h1>Proof-of-concept for Wohnungsbot</h1>
+        Migrating to <code>electron-forge</code> with <code>vite</code>...
+    </div>
+}
+
+export default App
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,4 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config
-export default defineConfig({});
+export default defineConfig({
+    plugins: [react()]
+});
