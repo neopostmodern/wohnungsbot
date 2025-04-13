99de9dda4d70b63f2d18d1d3f47b087a70797cc9
Georg Hoelger <georghoelger@gmx.de>
2025-04-13 17:03:50 +0200

    fix: use new selector

--- app/utils/selectors.ts
+++ app/utils/selectors.ts
@@ -1,4 +1,4 @@
 export const entrySelector = (entryId: string): string =>
   `[data-id="${entryId}"]`;
 export const entryTitleSelector = (entryId: string): string =>
-  `[data-go-to-expose-id="${entryId}"] > h2`;
+  `[data-exp-id="${entryId}"] > h2`;
