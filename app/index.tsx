c60fd74448818dc1252e638cdcdb992268f0fa59
Georg Hoelger <georghoelger@gmx.de>
2025-04-13 13:41:53 +0200

    DOUBLE CHECK remove __TARGET__

--- app/index.tsx
+++ app/index.tsx
@@ -4,12 +4,11 @@ import { RENDERER } from './constants/targets';
 import Root from './containers/Root';
 import getHistory from './store/history';
 import './styles/app.global.scss';
-// eslint-disable-next-line no-undef
-const target = typeof __TARGET__ === 'undefined' ? RENDERER : __TARGET__;
+
 import('./store/configureStore')
-  .then(({ default: configureStore }) =>
-    configureStore(target, process.env.NODE_ENV === 'development')
-  )
+  .then(({ default: configureStore }) => {
+    return configureStore(RENDERER, process.env.NODE_ENV === 'development');
+  })
   /* eslint-disable promise/always-return */
   .then((store) => {
     const history = getHistory();
